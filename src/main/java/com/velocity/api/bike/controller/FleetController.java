0f79014fee190f6d324fff2731a5b62fab3ef29e
JakubJurkian <kuba.jur03@gmail.com>
2026-07-06 11:13:18 +0200

    feat: add openAPI & Swagger with conf class

--- src/main/java/com/velocity/api/bike/controller/FleetController.java
+++ src/main/java/com/velocity/api/bike/controller/FleetController.java
@@ -3,6 +3,9 @@ package com.velocity.api.bike.controller;
 import com.velocity.api.bike.dto.BikeInstanceDto;
 import com.velocity.api.bike.service.FleetService;
 import com.velocity.api.shared.dto.PaginatedResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/fleet")
+@Tag(name = "Fleet Management", description = "Endpoints for interacting with the physical e-bike fleet")
 public class FleetController {
     private final FleetService fleetService;
 
@@ -20,7 +24,11 @@ public class FleetController {
     }
 
     @GetMapping
-    public ResponseEntity<PaginatedResponse<BikeInstanceDto>> getAvailableBikes(Pageable pageable) {
+    @Operation(
+            summary = "Retrieve available bikes",
+            description = "Returns a paginated list of all e-bikes currently marked with an ACTIVE status."
+    )
+    public ResponseEntity<PaginatedResponse<BikeInstanceDto>> getAvailableBikes(@ParameterObject Pageable pageable) {
         Page<BikeInstanceDto> bikePage = fleetService.getAvailableBikes(pageable);
 
         PaginatedResponse.PaginationMeta meta = new PaginatedResponse.PaginationMeta(
