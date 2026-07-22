cbc44e6beeddcdb818b1e8069900f2da0abd83e4
JakubJurkian <kuba.jur03@gmail.com>
2026-07-22 15:05:22 +0200

    chore: move business logic check to constructor from method | remove redundant save method from service | remove unused import from controller

--- src/main/java/com/velocity/api/billing/RentalCostCalculator.java
+++ src/main/java/com/velocity/api/billing/RentalCostCalculator.java
@@ -10,18 +10,16 @@ public class RentalCostCalculator {
     private final BigDecimal dailyFlatRate;
 
     public RentalCostCalculator(@Value("${pricing.daily-rate}") BigDecimal dailyFlatRate) {
+        if (dailyFlatRate.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("The flat rate should be greater a non-negative value.");
+        }
         this.dailyFlatRate = dailyFlatRate;
     }
 
     public BigDecimal calculate(int rentalDays) {
-        if(rentalDays <= 0) {
+        if (rentalDays <= 0) {
             throw new IllegalArgumentException("The rental days should be greater than 0.");
         }
-
-        if(dailyFlatRate.compareTo(BigDecimal.ZERO) < 0) {
-            throw new IllegalArgumentException("The flat rate should be greater a non-negative value.");
-        }
-
         return this.dailyFlatRate.multiply(BigDecimal.valueOf(rentalDays));
     }
 }
