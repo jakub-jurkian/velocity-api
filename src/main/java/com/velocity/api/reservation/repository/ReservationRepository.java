b7227f1ff82b19b1e3b59fb84318efb8498fd5b1
JakubJurkian <kuba.jur03@gmail.com>
2026-07-29 15:49:36 +0200

    feat: add reservation controller, create reservation method to service and custom sql query to repository

--- src/main/java/com/velocity/api/reservation/repository/ReservationRepository.java
+++ src/main/java/com/velocity/api/reservation/repository/ReservationRepository.java
@@ -2,8 +2,21 @@ package com.velocity.api.reservation.repository;
 
 import com.velocity.api.reservation.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 public interface ReservationRepository extends JpaRepository<Reservation, UUID> {
+    @Query("""
+              SELECT EXISTS(
+                SELECT 1 FROM Reservation r
+                  WHERE r.bikeInstance.id = :bikeId
+                    AND r.status IN ('PENDING', 'CONFIRMED')
+                    AND r.startDate < :reqEndDate
+                    AND r.endDate > :reqStartDate
+              )
+    """)
+    public boolean isBikeAvailable(@Param("bikeId") UUID bikeId, @Param("reqStartDate") LocalDate reqStartDate, @Param("reqEndDate") LocalDate reqEndDate);
 }
