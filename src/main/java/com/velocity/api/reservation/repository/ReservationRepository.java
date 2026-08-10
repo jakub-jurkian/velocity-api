114ecad7d63f888ee655967844302bccd5901c7b
JakubJurkian <kuba.jur03@gmail.com>
2026-08-10 16:07:56 +0200

    feat: create scheduler class for two background jobs | add related to bg jobs methods to service & repository layers | add @EnableScheduling to main class of the app

--- src/main/java/com/velocity/api/reservation/repository/ReservationRepository.java
+++ src/main/java/com/velocity/api/reservation/repository/ReservationRepository.java
@@ -1,22 +1,31 @@
 package com.velocity.api.reservation.repository;
 
 import com.velocity.api.reservation.Reservation;
+import com.velocity.api.reservation.ReservationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.Instant;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public interface ReservationRepository extends JpaRepository<Reservation, UUID> {
     @Query("""
-              SELECT NOT EXISTS(
-                SELECT 1 FROM Reservation r
-                  WHERE r.bikeInstance.id = :bikeId
-                    AND r.status IN ('PENDING', 'CONFIRMED')
-                    AND r.startDate < :endDate
-                    AND r.endDate > :startDate
-              )
-    """)
+                      SELECT NOT EXISTS(
+                        SELECT 1 FROM Reservation r
+                          WHERE r.bikeInstance.id = :bikeId
+                            AND r.status IN ('PENDING', 'CONFIRMED')
+                            AND r.startDate < :endDate
+                            AND r.endDate > :startDate
+                      )
+            """)
     public boolean isBikeAvailable(@Param("bikeId") UUID bikeId, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+
+    @Query("SELECT r.id FROM Reservation r WHERE r.status = :status AND r.createdAt < :cutoff")
+    public List<UUID> findStalePendingReservationsIds(@Param("status") ReservationStatus status, @Param("cutoff") Instant cutoff);
+
+    @Query("SELECT r.id FROM Reservation r WHERE r.status = :status AND r.endDate < :today")
+    public List<UUID> findPastDueConfirmedReservationsIds(@Param("status") ReservationStatus status, @Param("today") LocalDate today);
 }
---
+++ src/main/java/com/velocity/api/reservation/scheduler/ReservationLifecycleScheduler.java
@@ -0,0 +1,58 @@
+package com.velocity.api.reservation.scheduler;
+
+import com.velocity.api.reservation.ReservationStatus;
+import com.velocity.api.reservation.repository.ReservationRepository;
+import com.velocity.api.reservation.service.ReservationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ReservationLifecycleScheduler {
+    private final ReservationRepository reservationRepository;
+    private final ReservationService reservationService;
+
+    // @Scheduled(fixedRate = 60000)
+    @Scheduled(fixedRate = 10000)
+    public void cancelStalePendingReservations() {
+        Instant cutoff = Instant.now().minus(30, ChronoUnit.MINUTES);
+        List<UUID> stalePendingReservationIds = reservationRepository.findStalePendingReservationsIds(ReservationStatus.PENDING, cutoff);
+
+        for (UUID id : stalePendingReservationIds) {
+            try {
+                reservationService.cancelStaleReservation(id);
+            } catch (ObjectOptimisticLockingFailureException e) {
+                log.warn("Reservation {} already modified.", id);
+            } catch (Exception e) {
+                log.error("Unexpected error processing reservation {}", id, e);
+            }
+        }
+    }
+
+    // @Scheduled(cron = "0 1 0 * * *")
+    @Scheduled(fixedRate = 10000) // for testing
+    public void completePastDueConfirmedReservations() {
+
+        List<UUID> pastDueConfirmedReservationIds = reservationRepository.findPastDueConfirmedReservationsIds(ReservationStatus.CONFIRMED, LocalDate.now());
+
+        for (UUID id : pastDueConfirmedReservationIds) {
+            try {
+                reservationService.completePastDueConfirmedReservations(id);
+            } catch (ObjectOptimisticLockingFailureException e) {
+                log.warn("Reservation {} already modified.", id);
+            } catch (Exception e) {
+                log.error("Unexpected error processing reservation {}", id, e);
+            }
+        }
+    }
+}
