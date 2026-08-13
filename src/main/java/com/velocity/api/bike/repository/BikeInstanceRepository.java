888991083936059ba8d8e04e788016756cfab10c
Jakub Jurkian <84335279+jakub-jurkian@users.noreply.github.com>
2026-08-13 11:13:03 +0200

    Fix (#54): n+1 and seed data problem
    * fix: set seeding data properly | set batch for scheduler queries to eliminate N+1 write
    
    * fix: add @EntityGraph to avoid N+1 read
    
    * Add Liquibase context for test environment

--- src/main/java/com/velocity/api/bike/repository/BikeInstanceRepository.java
+++ src/main/java/com/velocity/api/bike/repository/BikeInstanceRepository.java
@@ -6,6 +6,7 @@ import com.velocity.api.bike.repository.projection.AvailableModelProjection;
 import org.springframework.cglib.core.Local;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,6 +16,7 @@ import java.util.List;
 import java.util.UUID;
 
 public interface BikeInstanceRepository extends JpaRepository<BikeInstance, UUID> {
+    @EntityGraph(attributePaths = {"bikeModel"})
     Page<BikeInstance> findByStatus(BikeStatus status, Pageable pageable);
 
     @Query(nativeQuery = true, value = """
