510a53d0d21262b6bcc1c76db157ac8a11a44225
Jakub Jurkian <84335279+jakub-jurkian@users.noreply.github.com>
2026-08-31 12:56:30 +0200

    Feat/update fetching av bike models (#76)
    * feat: add reservation endpoint within mvc layers & update PROJECT_CONTEXT file
    
    * feat: add logic for fetching only models which are available in user's city

--- src/main/java/com/velocity/api/bike/repository/BikeInstanceRepository.java
+++ src/main/java/com/velocity/api/bike/repository/BikeInstanceRepository.java
@@ -3,6 +3,7 @@ package com.velocity.api.bike.repository;
 import com.velocity.api.bike.BikeInstance;
 import com.velocity.api.bike.BikeStatus;
 import com.velocity.api.bike.repository.projection.AvailableModelProjection;
+import com.velocity.api.common.City;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
@@ -19,10 +20,10 @@ public interface BikeInstanceRepository extends JpaRepository<BikeInstance, UUID
     Page<BikeInstance> findByStatus(BikeStatus status, Pageable pageable);
 
     @Query(nativeQuery = true, value = """
-                SELECT DISTINCT ON (m.id) i.id AS bookableInstanceId, m.name AS modelName, m.description AS modelDescription, m.speed AS modelSpeed, m.range AS modelRange, m.capacity AS modelCapacity, m.category AS modelCategory FROM bike_instances i JOIN bike_models m ON i.bike_model_id = m.id WHERE i.status = 'ACTIVE' AND NOT EXISTS (SELECT 1 FROM reservations r WHERE r.bike_instance_id = i.id
-                AND r.status IN ('PENDING', 'CONFIRMED')
+                SELECT DISTINCT ON (m.id) i.id AS bookableInstanceId, m.name AS modelName, m.description AS modelDescription, m.speed AS modelSpeed, m.range AS modelRange, m.capacity AS modelCapacity, m.category AS modelCategory FROM bike_instances i JOIN bike_models m ON i.bike_model_id = m.id WHERE i.status = 'ACTIVE' AND i.city = :city AND NOT EXISTS (SELECT 1 FROM reservations r WHERE r.bike_instance_id = i.id
+                AND r.status IN ('PENDING', 'CONFIRMED')                                                                                                                                                                                                                                                                                                   \s
                 AND daterange(CAST(:startDate AS date), CAST(:endDate AS date), '[)') && daterange(r.start_date, r.end_date, '[)')
-                );
-            """)
-    List<AvailableModelProjection> findAvailableModels(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+                ) ORDER BY m.id, i.id;
+           \s""")
+    List<AvailableModelProjection> findAvailableModels(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate, @Param("city") String city);
 }
