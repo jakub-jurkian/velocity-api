9db49721a88bb424784c94b94a8f5a2ab0632bdf
Jakub Jurkian <84335279+jakub-jurkian@users.noreply.github.com>
2026-09-03 11:29:55 +0200

    feat: add reservation constraints (#83)

--- src/main/java/com/velocity/api/pricing/RentalCostCalculator.java
+++ src/main/java/com/velocity/api/pricing/RentalCostCalculator.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Component
 public class RentalCostCalculator {
@@ -17,9 +18,17 @@ public class RentalCostCalculator {
     }
 
     public BigDecimal calculate(int rentalDays) {
+        BigDecimal standardCost = this.dailyFlatRate.multiply(BigDecimal.valueOf(rentalDays));
+        final BigDecimal multiplier;
         if (rentalDays <= 0) {
             throw new IllegalArgumentException("The rental days should be greater than 0.");
+        } else if (rentalDays <= 7) {
+            multiplier = BigDecimal.ONE;
+        } else if (rentalDays <= 14) {
+            multiplier = new BigDecimal("0.80");
+        } else {
+            multiplier = new BigDecimal("0.60");
         }
-        return this.dailyFlatRate.multiply(BigDecimal.valueOf(rentalDays));
+        return standardCost.multiply(multiplier).setScale(2, RoundingMode.HALF_UP);
     }
 }
--- src/main/java/com/velocity/api/reservation/dto/ReservationBookRequest.java
+++ src/main/java/com/velocity/api/reservation/dto/ReservationBookRequest.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.UUID;
 
 public record ReservationBookRequest(
@@ -19,8 +20,15 @@ public record ReservationBookRequest(
         LocalDate endDate) {
     @AssertTrue(message = "End date must be strictly after start date")
     // if false, validator immediately halts execution and throws a MethodArgumentNotValidException.
-    public boolean isValidDateRange() {
+    public boolean isValidDateOrder() {
         if (startDate == null || endDate == null) return true;
         return endDate.isAfter(startDate);
+    }
+
+    @AssertTrue(message = "Rental duration must be between 3 and 21 days")
+    public boolean isDurationValid() {
+        if (startDate == null || endDate == null) return true;
+        long days = ChronoUnit.DAYS.between(startDate, endDate);
+        return days >= 3 && days <= 21;
     }
 }
